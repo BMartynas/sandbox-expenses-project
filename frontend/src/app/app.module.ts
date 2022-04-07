deb64fbbc2caaf0071ead69389d0d10e02ff047e
mabi5659 <binkis.martynas@gmail.com>
2022-04-07 11:26:54 +0300

    relocated notFoundComponent

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NotFoundComponent } from './not-found/not-found.component';
 import { AuthModule } from './auth/auth.module';
 import { MainModule } from './main/main.module';
 import { SharedModule } from './shared/shared.module';
@@ -12,7 +11,7 @@ import { AuthInterceptor } from './auth/interceptors/auth.interceptor';
 import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
-  declarations: [AppComponent, NotFoundComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     SharedModule,
